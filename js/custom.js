8c887c4221d62f4bd30db5583e65d5c6a13dde7d
desipossa <c1jang@hanmail.net>
2024-06-11 09:12:09 +0900

    dokdo swiper

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('#header').addClass('on')
+        } else {
+            $('#header').removeClass('on')
+        }
+    });
+
+
+    const mainVisualSlide = new Swiper('.main_visual_slide', {
+        parallax: true,
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
+})
